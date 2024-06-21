4da498d34b69b154386df264bb93a87eda057cc1
Alhim <alhimic1551@gmail.com>
2024-06-21 20:12:41 +0300

    Day 8(start)

--- service/service/settings.py
+++ service/service/settings.py
@@ -145,3 +145,14 @@ LOGGING = {
 }
 
 CELERY_BROKER_URL = 'redis://redis:6379/0'
+
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": 'redis://redis:6379',
+        "OPTIONS": {
+            "db": "1",
+        }
+    }
+}
